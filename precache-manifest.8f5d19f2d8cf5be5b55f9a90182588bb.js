06ecf369516a0cfc986cd917ad44e1ffd533f908
Martin J Dixon <martinjdixon@Martins-MacBook-Pro.local>
2020-10-19 10:59:48 +0000

    Updates

---
+++ precache-manifest.8f5d19f2d8cf5be5b55f9a90182588bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "005061f8a04d5271117026753b641cb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e125dd18f1f21343b7da",
+    "url": "/static/css/main.0c571e4d.chunk.css"
+  },
+  {
+    "revision": "df86403b3f84c3c9ca52",
+    "url": "/static/js/2.5b56c4fc.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.5b56c4fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e125dd18f1f21343b7da",
+    "url": "/static/js/main.c0623db6.chunk.js"
+  },
+  {
+    "revision": "68c9e8550e2e29b6cf5c",
+    "url": "/static/js/runtime-main.d4f53fb3.js"
+  }
+]);
